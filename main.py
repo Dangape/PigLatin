a0c0a98ee8c83c8e967006446ad5a40b5bf854de
Daniel Aben-Athar Bemerguy <daniel06197@gmail.com>
2021-11-13 12:08:08 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,19 +9,23 @@ punctuation = string.punctuation
 # Input test string here
 s = 'Hey buddy, get away from my car!'
 
+# function to find all vowel indexes in a word
 def vowel_index(string):
     for index, letter in enumerate(string):
         if letter in vowel:
             return index
 
+# function to find word´s prefix
 def prefix(string):
     index = vowel_index(string)
     return string[:index]
 
+# function to find word´s stem
 def stem(string):
     index = vowel_index(string)
     return string[index:]
 
+# function to count consonants
 def count_consonant(string):
     count = 0
     s = string.lower()
@@ -30,13 +34,14 @@ def count_consonant(string):
             count += 1
     return count
 
+# function to translate word
 def translate(string):
     translated = []
     s = re.findall(r"[\w']+|[.,!?;]", string) #split string in spaces and punctuation
     for word in s:
-        if word.isalpha():
+        if word.isalpha(): #check if word contains only letters
             consonant_count = count_consonant(word)
-            if consonant_count == 0:
+            if consonant_count == 0: # word only contains consonants
                 pref = ''
                 st = stem(word)
                 translated_string = st + pref + 'yay'
